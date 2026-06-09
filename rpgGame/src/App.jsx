c753e0c451d35f78dbd3e5cdf110d247b02e5ca7
Scottie113 <sdm6f8@mail.missouri.edu>
2026-06-08 20:41:20 -0500

    Finished TopBar with info bar

--- rpgGame/src/App.jsx
+++ rpgGame/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import GameLayout from './assets/components/layout/GameLayout'
+import TopBar from './assets/components/layout/TopBar'
+import BottomPanel from './assets/components/layout/BottomPanel'
 
 import './App.css'
 
 function App() {
   return (
     <div>
+        <TopBar />
       <GameLayout />
+      <BottomPanel />
     </div>
   )
 }
--- rpgGame/src/assets/components/layout/TopBar.jsx
+++ rpgGame/src/assets/components/layout/TopBar.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import './topBar.css'
+import { useState } from 'react'
+import './TopBar.css'
 
 function TopBar() {
+        const [health, setHealth] = useState(100)
+        const [lightning, setLightning] = useState(50)
+        const [gold, setGold] = useState(0)
+        const [level, setLevel] = useState(1)
+
   return (
-    <div>
-      
-    </div>
+    <nav className="top-bar">
+
+   
+      <ul>
+        <li>Active Quest: </li>
+        <li>
+           <button>Map</button>
+        </li>
+        <li>
+            <button>Inventory</button>
+        </li>
+        <li>
+            <button>Quest</button>
+        </li>
+        <li>
+            <button>Shops</button>
+        </li>
+
+      </ul>
+
+    </nav>
   )
 }
 
