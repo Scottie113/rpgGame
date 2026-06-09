8dbc85030a9105a9ec54276781f000e90f350708
Scottie113 <sdm6f8@mail.missouri.edu>
2026-06-08 20:21:05 -0500

    top bar set up

--- rpgGame/src/App.jsx
+++ rpgGame/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import GameLayout from './assets/components/layout/GameLayout'
+
 import './App.css'
 
 function App() {
-
+  return (
+    <div>
+      <GameLayout />
+    </div>
+  )
 }
 
 export default App
--- rpgGame/src/assets/components/layout/GameLayout.jsx
+++ rpgGame/src/assets/components/layout/GameLayout.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
-import './gameLayout.css'
+import { useState } from 'react'
+import './GameLayout.css'
+import image from '../../images/istockphoto-1416937202-612x612.jpg' // Adjust the path to your image file
+import healthIcon from '../../images/images.png' // Adjust the path to your health icon 
+import lightningIcon from '../../images/lightning.png' // Adjust the path to your lightning icon
+
 
 function GameLayout() {
+
+    const [health, setHealth] = useState(100)
+    const [lightning, setLightning] = useState(50)
+    const [gold, setGold] = useState(0)
+    const [level, setLevel] = useState(1)
+
   return (
-    <div>
-      
-    </div>
+    <nav className="top-bar">
+
+   
+      <ul>
+        <li>Fighter Name</li>
+        <li>
+            <img src={healthIcon} alt="Health Icon" />
+            Health: {health}
+        </li>
+        <li>
+            <img src={lightningIcon} alt="Lightning Icon" />
+            Lightning: {lightning}
+        </li>
+        <li className="rightcol">
+          <span>Gold: {gold}</span>
+          <span>Level: {level}</span>
+          <img src={image} alt="Description" />
+        </li>
+
+      </ul>
+
+    </nav>
   )
 }
 
